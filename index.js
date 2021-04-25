8ddebee63e2769ceff072cc095954c9e1b79d4e7
VictoriaBench <victoria.bench@gmail.com>
2021-04-25 09:33:56 +0200

    Can add a group to a hardcoded group array through a UI

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+
+// loading routers
 const mainRouter = require('./src/routes/mainRoutes')
+// const groupRouter = require('./src/routes/groupRoutes')
+
 const db = require('./src/db')
+
 const app = express()
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
 const port = 3000
 
 app.use(mainRouter)
+// app.use(groupRouter)
 app.listen(port)
 
 console.log('Blast it Chewie 💫 Express server running on port', port)
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,36 @@
+
 const express = require('express')
 const mainRouter = express.Router()
 
+const groups = [
+  {
+    name: 'Study Group 1',
+    members: ['Tori', 'Duncan', 'Blake']
+  },
+
+  {
+    name: 'Study Group 2',
+    members: ['Jon', 'Josh', 'Jess Spatula']
+  }
+]
+
 mainRouter.get('/', function (req, res) {
   res.send('Hello Express')
+})
+
+mainRouter.get('/groups', function (req, res) {
+  res.render('groups', { groups: groups })
+})
+
+mainRouter.post('/groups', function (req, res) {
+  const name = req.body.name
+  const newGroup = { name: name, members: [] }
+  groups.push(newGroup)
+  res.redirect('/groups')
+})
+
+mainRouter.get('/groups/new', function (req, res) {
+  res.render('newGroup.ejs')
 })
 
 module.exports = mainRouter
