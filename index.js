241c336c33ebc699ff73e97b3b0a586c29412bd0
Blake-Denham <blake.denham@gmail.com>
2021-04-25 09:58:19 +0200

    added ejs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const mainRouter = require('./src/routes/mainRoutes')
+const meetingRouter = require('./src/routes/meetingsRoutes')
 const db = require('./src/db')
 const app = express()
+const path = require('path')
+const ejsMate = require('ejs-mate');
 
 const port = 3000
 
-app.use(mainRouter)
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/',mainRouter)
+app.use('/meetings', meetingRouter)
 app.listen(port)
 
 console.log('Blast it Chewie 💫 Express server running on port', port)
