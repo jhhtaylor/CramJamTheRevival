3fc378642c155c1c881eb814c8fdbfd06e18a70a
VictoriaBench <victoria.bench@gmail.com>
2021-04-25 19:02:08 +0200

    remove edit tests

---
+++ src/controllers/groups.js
@@ -0,0 +1,31 @@
+// Private
+const list = [
+  {
+    name: 'Study Group 1',
+    members: ['Tori', 'Blake', 'Duncan']
+  },
+  {
+    name: 'Study Group 2',
+    members: ['Jon', 'Josh', 'Jess']
+  }
+
+]
+// Public
+
+module.exports = {
+  add: function (name) {
+    const newGroup = { name: name, members: [] }
+    list.push(newGroup)
+  },
+
+  list: function () {
+    return list
+  },
+
+  deleteMember: function (groupName, studentName) {
+    const groupIndex = list.findIndex(item => item.name === groupName)
+    const studentIndex = list[groupIndex].members.indexOf(studentName)
+    list[groupIndex].members.splice(studentIndex, 1)
+  }
+
+}
