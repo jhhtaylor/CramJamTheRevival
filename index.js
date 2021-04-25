35af412eb2e266fe8344a03985c2a9ab48789186
Blake-Denham <blake.denham@gmail.com>
2021-04-25 10:35:42 +0200

    names

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mainRouter = require('./src/routes/mainRoutes')
-const meetingRouter = require('./src/routes/meetingsRoutes')
 const db = require('./src/db')
 const app = express()
 const path = require('path')
@@ -13,8 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/',mainRouter)
-app.use('/meetings', meetingRouter)
+app.use(mainRouter)
 app.listen(port)
 
 console.log('Blast it Chewie 💫 Express server running on port', port)
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const mainRouter = express.Router()
+const meetings = require('../controllers/meetings');
 
 mainRouter.get('/', function (req, res) {
   res.send('Hello Express')
 })
+mainRouter.route('/meetings')
+  .get(meetings.add)
+
 
 module.exports = mainRouter
