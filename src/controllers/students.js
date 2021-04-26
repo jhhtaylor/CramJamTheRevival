9b87457e6c921908154a0fec6001652f8462c2ff
DuncanSmale <duncan.smale@gmail.com>
2021-04-26 22:35:12 +0200

    Added basic registration form and logic

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,14 +1,26 @@
+const { StudentProfile } = require('../db/studentProfiles')
+
 const students = [{
-    email: "blake@email.com",
-    firstName: "Blake",
-    lastName: "Denham",
-    password: "Awe"
+  email: 'blake@email.com',
+  firstName: 'Blake',
+  lastName: 'Denham',
+  password: 'Awe'
 }]
 
 module.exports.add = (newStudent) => {
-    students.push(newStudent)
+  students.push(newStudent)
 }
 
 module.exports.list = () => {
-    return students
+  return students
+}
+
+module.exports.renderRegisterStudent = (req, res) => {
+  res.render('students/register')
+}
+
+module.exports.registerStudent = async (req, res) => {
+  const { email, firstName, lastName, password } = req.body
+  const newStudent = new StudentProfile({ email, firstName, lastName })
+  StudentProfile.register(newStudent, password)
 }
