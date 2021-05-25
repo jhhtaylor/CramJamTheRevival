2578393bcd1b3df8a1f59a1ca8cb02563cc0eb5c
DuncanSmale <duncan.smale@gmail.com>
2021-05-25 18:06:37 +0200

    Routes working and added exposure fields

--- src/controllers/survey.js
+++ src/controllers/survey.js
@@ -1,5 +1,7 @@
 module.exports.takeSurvey = async (req, res) => {
-
+  const { symptoms, exposure } = req.body
+  console.log(symptoms, exposure)
+  res.redirect('/survey')
 }
 
 module.exports.showSurvey = (req, res) => {
