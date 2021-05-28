3d2386e610427706d47bcb4afdfcbd599541e7e9
DuncanSmale <duncan.smale@gmail.com>
2021-05-28 19:01:03 +0200

    Implemented survey table and implemented tests

--- src/controllers/survey.js
+++ src/controllers/survey.js
@@ -1,7 +1,15 @@
+const { CovidSurvey } = require('../db/covidSurvey')
+
 module.exports.takeSurvey = async (req, res) => {
   const { symptoms, exposure } = req.body
-  console.log(symptoms, exposure)
-  res.redirect('/survey')
+  const newSurvey = new CovidSurvey({
+    student: req.user._id,
+    symptoms,
+    exposure
+  })
+  await newSurvey.save()
+  req.flash('success', 'Covid Survey Taken')
+  res.redirect('/')
 }
 
 module.exports.showSurvey = (req, res) => {
