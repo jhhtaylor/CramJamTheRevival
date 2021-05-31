775af7077fd97f57e117c704afbdbeb1990068ab
Jonathan Taylor <jhhtaylor@gmail.com>
2021-05-31 12:07:41 +0200

    Can view link items

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -10,4 +10,16 @@ const router = express.Router()
 router.route('/')
   .get(isLoggedIn, catchAsync(links.index))
 
+router.post('/', function (req, res) {
+  const { name, url } = req.body
+  const newLink = { name: name, url: url }
+  links.push(newLink)
+
+  res.redirect('/')
+})
+
+router.get('/new', function (req, res) {
+  res.render('links/new.ejs')
+})
+
 module.exports = router
