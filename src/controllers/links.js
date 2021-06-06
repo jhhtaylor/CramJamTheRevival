bcc83e99674fc665b0a8c5e86ae81a1b637b3580
Jonathan Taylor <jhhtaylor@gmail.com>
2021-06-06 14:57:40 +0200

    Added creating links from database

--- src/controllers/links.js
+++ src/controllers/links.js
@@ -1,29 +1,3 @@
-// temporary hard-coded items
-// module.exports.linkItems = [
-//   {
-//     // user: 'user1',
-//     // group: 'Group 1',
-//     name: 'Battery Low Voltage Alarm',
-//     url:
-//       'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
-//   },
-//   {
-//     // user: 'user2',
-//     // group: 'Group 2',
-//     name: 'Marxism',
-//     url:
-//       'https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/01/87/21/018721c6-551b-5d8d-1b74-bedaad367533/source/256x256bb.jpg',
-//   },
-//   {
-//     // user: 'user3',
-//     // group: 'Group 3',
-//     name: 'Radical Equations',
-//     url:
-//       'https://cdn.kastatic.org/ka-exercise-screenshots-2/xf46f88061126b980.png',
-//   }
-// ]
-// db.links.insert({name: "", url: ""})
-
 const { LinkSchema } = require('../db/links')
 
 module.exports.index = async (req, res) => {
@@ -31,17 +5,16 @@ module.exports.index = async (req, res) => {
   res.render('links/index', { linkItems: links })
 }
 
-// module.exports.index = async (req, res) => {
-//   res.render('links/index', { linkItems: this.linkItems })
-// }
-// module.exports.createLink = async (req, res) => {
-//   const { name, url } = req.body
-//   const newLink = { name: name, url: url }
-//   this.linkItems.push(newLink)
+module.exports.renderNewForm = async (req, res) => {
+  res.render('links/new.ejs')
+}
 
-//   res.redirect('/links')
-// }
+module.exports.createLink = async (req, res) => {
+  const link = new LinkSchema({
+    name: req.body.name,
+    url: req.body.url
+  })
+  await link.save()
 
-// module.exports.renderNewForm = async (req, res) => {
-//   res.render('links/new.ejs')
-// }
+  res.redirect('/links')
+}
