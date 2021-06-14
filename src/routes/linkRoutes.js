6585229f0747ab4514e7a287fbc2759fa4ecc8f0
DuncanSmale <duncan.smale@gmail.com>
2021-06-14 12:57:38 +0200

    Added links and meeting group name

--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -3,17 +3,16 @@ const express = require('express')
 const catchAsync = require('../../utils/catchAsync')
 const { isLoggedIn } = require('../middleware/middleware')
 const links = require('../controllers/links')
+const { body } = require('express-validator')
 
 const router = express.Router()
 
-router.route('/')
-  .get(isLoggedIn, catchAsync(links.index))
+router.route('/').get(isLoggedIn, catchAsync(links.index))
 
-router.route('/new')
-  .get(isLoggedIn, catchAsync(links.renderNewForm))
+router.route('/new').get(isLoggedIn, catchAsync(links.renderNewForm))
 
-router.route('/')
-  .post(isLoggedIn, catchAsync(links.createLink))
-
+router
+  .route('/')
+  .post(isLoggedIn, body('name').escape().trim(), catchAsync(links.createLink))
 
 module.exports = router
